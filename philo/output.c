5d874a928a27c5cae397d519a2b888936ab06b1d
ismaelucky342 <ismaellhc342@gmail.com>
2024-10-23 16:34:04 +0200

    a

--- philo/output.c
+++ philo/output.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   output.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apollo <apollo@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ismherna <ismherna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 21:37:15 by ismherna          #+#    #+#             */
-/*   Updated: 2024/10/23 12:43:58 by apollo           ###   ########.fr       */
+/*   Updated: 2024/10/23 16:22:17 by ismherna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ static void	print_eat(t_global_info *data)
 	pthread_mutex_lock(data->mutex_l_ate);
 	data->last_ate = ft_get_time() - data->start_time;
 	pthread_mutex_unlock(data->mutex_l_ate);
-	printf(COLOR_GREEN "%lld %d is eating\n" COLOR_RESET, data->last_ate, data->philo);
+	printf(COLOR_GREEN "%lld %d is eating\n" COLOR_RESET, data->last_ate,
+		data->philo);
 	pthread_mutex_lock(data->mutex_to_eats);
 	data->number_eats[data->philo - 1] += 1;
 	pthread_mutex_unlock(data->mutex_to_eats);
@@ -26,7 +27,8 @@ static void	print_eat(t_global_info *data)
 void	ft_print_death(t_global_info *data)
 {
 	pthread_mutex_lock(data->mutex_to_print);
-	printf(COLOR_RED "%lld %d died\n" COLOR_RESET, ft_get_time() - data->start_time, data->philo);
+	printf(COLOR_RED "%lld %d died\n" COLOR_RESET, ft_get_time()
+		- data->start_time, data->philo);
 	pthread_mutex_unlock(data->mutex_to_print);
 }
 
@@ -41,11 +43,11 @@ void	ft_print(t_global_info *data, int action)
 		else if (action == EAT)
 			print_eat(data);
 		else if (action == SLEEP)
-			printf(COLOR_BLUE "%lld %d is sleeping\n" COLOR_RESET,
-				ft_get_time() - data->start_time, data->philo);
+			printf(COLOR_BLUE "%lld %d is sleeping\n" COLOR_RESET, ft_get_time()
+				- data->start_time, data->philo);
 		else if (action == THINK)
-			printf(COLOR_CYAN "%lld %d is thinking\n" COLOR_RESET,
-				ft_get_time() - data->start_time, data->philo);
+			printf(COLOR_CYAN "%lld %d is thinking\n" COLOR_RESET, ft_get_time()
+				- data->start_time, data->philo);
 	}
 	pthread_mutex_unlock(data->mutex_to_print);
 }
